0cab02536e954fb668c9a985f3305c33851adfd4
RolexSir30 <141523629+RolexSir30@users.noreply.github.com>
2025-08-06 16:42:07 +0200

    Update CsvtoDbApplication.java

--- src/main/java/csvtoDB/csvtoDB/CsvtoDbApplication.java
+++ src/main/java/csvtoDB/csvtoDB/CsvtoDbApplication.java
@@ -1,12 +1,14 @@
 package csvtoDB.csvtoDB;
 
-
-
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class CsvtoDbApplication implements CommandLineRunner {
@@ -17,6 +19,9 @@ public class CsvtoDbApplication implements CommandLineRunner {
     @Autowired
     private Job hibernateJob;
 
+    @Autowired
+    private MeterRegistry meterRegistry;
+
     public static void main(String[] args) {
         SpringApplication.run(CsvtoDbApplication.class, args);
     }
@@ -26,6 +31,20 @@ public class CsvtoDbApplication implements CommandLineRunner {
         JobParameters params = new JobParametersBuilder()
                 .addString("JobID", String.valueOf(System.currentTimeMillis()))
                 .toJobParameters();
-        jobLauncher.run(hibernateJob, params);
+
+        Timer.Sample sample = Timer.start(meterRegistry);
+        try {
+            JobExecution execution = jobLauncher.run(hibernateJob, params);
+            if (execution.getStatus() == BatchStatus.FAILED) {
+                meterRegistry.counter("batch.job.failure", "job", hibernateJob.getName()).increment();
+            } else {
+                meterRegistry.counter("batch.job.success", "job", hibernateJob.getName()).increment();
+            }
+        } finally {
+            sample.stop(Timer.builder("batch.job.duration")
+                    .description("Duration of Spring Batch job")
+                    .tag("job", hibernateJob.getName())
+                    .register(meterRegistry));
+        }
     }
 }
